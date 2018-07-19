72bb53f55eb14172274e563c94c4198c25a0b680
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-07-19 16:40:41 -0400

    Edit Bird model, route, views

--- models/Bird.js
+++ models/Bird.js
@@ -3,17 +3,11 @@ const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const birdSchema = new Schema({
-  // name: {
-    // first: { type: String, set: capitalizeFirstLetter, required: true },
-    // last: { type: String, set: capitalizeFirstLetter, required: true }
-  // },
   name: { type: String },
   color: [ { type: String } ],
   dob: { type: Date },
   captures: { type: Number },
-  keeper: { type : Schema.Types.ObjectId, ref: 'Users', required:true },
-  // email: { type: String, unique: true, required: true },
-  // phone: String,
+  keeper: { type : Schema.Types.ObjectId, ref: 'Users', required: true },
   profilePic: { type: String, default: "/images/pig-default-avatar.jpg" }
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
---
+++ routes/bird.js
@@ -0,0 +1,48 @@
+const express       = require("express");
+const router        = express.Router();
+const User          = require('../models/User');
+const Bird          = require('../models/Bird');
+
+router.get('/add-bird', (req, res, next) => {
+  const name      = req.body.name;
+  const color     = req.body.color;
+  const dob       = req.body.dob;
+  const captures  = req.body.captures;
+  const keeper    = req.session.passport.user;
+
+  if (name === `` || color === ``) {
+    res.render(`/users/add-bird`, { message: `Indicate name and color` });
+    return;
+  }
+
+  Bird.findOne({ name })
+  .then(bird => {
+    if (bird !== null) {
+      res.render(`users/add-bird`, { message: `The bird already exists` });
+      return;
+    }
+
+    const newBird = new User({
+      name,
+      color,
+      dob,
+      captures,
+      keeper
+    });
+
+    newBird.save((err) => {
+      if (err) {
+        res.render(`users/add-bird`, { message: `Something went wrong ${err}` });
+      }
+    })
+    .then((birdFromDb) => {
+      console.log(`bird form db after adding bird`, birdFromDb);
+      res.redirect(`/users/dashboard`);
+    })
+  })
+  .catch(error => {
+    next(error)
+  })
+});
+
+module.exports = router;
